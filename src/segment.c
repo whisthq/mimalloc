6d9a8470233c8eef2f511f3f2c4bef4387b23abd
Serina Hu <serina_hu@college.harvard.edu>
2022-11-03 11:15:39 -0400

    page_munlock if not force_reset

--- src/segment.c
+++ src/segment.c
@@ -362,10 +362,11 @@ static void mi_pages_reset_remove_all_in_segment(mi_segment_t* segment, bool for
     mi_page_t* page = &segment->pages[i];
     if (!page->segment_in_use && page->is_committed && !page->is_reset) {
       mi_pages_reset_remove(page, tld);
-      // Serina: we must munlock these pages even if they are not reset, to prevent nested mlocks when the segment is reused
-      mi_page_munlock(segment, page, 0);
       if (force_reset) {
         mi_page_reset(segment, page, 0, tld);
+      } else {
+        // Serina: we must munlock these pages even if they are not reset, to prevent nested mlocks when the segment is reused
+        mi_page_munlock(segment, page, 0);
       }
     }
     else {
